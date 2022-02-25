d3ea30f7c3bf6884f5e2f39efbc8ed02d232186b
mariacappellano <merycappellanodocampo@live.com>
2022-02-25 15:36:09 -0300

    Eliminacion de comentarios extra

--- src/test/java/SeleniumTestLaptop.java
+++ src/test/java/SeleniumTestLaptop.java
@@ -33,60 +33,23 @@ public class SeleniumTestLaptop {
         laptops.click();
         //Le agrego un wait para darle tiempo a cargar el sitio
         WebElement primerProductoSonyVaio = new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.elementToBeClickable(By.linkText("Sony vaio i5")));
-        //primerProductoSonyVaio.getText();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //Obtengo el nombre del primer producto
         String nombrePrimerProduct = driver.findElement(By.linkText("Sony vaio i5")).getText();
+        //Muestro en consola para verificar que se carga bien
         System.out.println(nombrePrimerProduct);
 
-        //System.out.println(primerProductoSonyVaio);
-        //Declaro el nombre del producto como String para poder obtener el valor con un getText()
-        //String nombreProductoSony = driver.findElement(By.linkText("Sony vaio i5")).getText();
-        //WebElement nombreProductoSony = new WebDriverWait(driver, Duration.ofSeconds(10)).until(driver -> driver.findElement(By.linkText("Sony vaio i5")));
-        //assertEquals(nombreProductoSony.getText(), "Sony vaio i5");
-        //Obtengo el nombre del producto
-        //System.out.println(nombreProductoSony);
-
-
-        //Una vez que carga el sitio, le doy click al elemento indicado
-        //primerProductoSonyVaio.click();
-
-
-        //sleep(1000);
-        //Declaro el precio del producto como String para poder obtener el valor con un getText()
-        //String precioProductoSony = driver.findElement(By.cssSelector("body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h5:nth-child(2)")).getText();
-       // String precioVaio = driver.findElement(By.className("price-container")).getText();
-        //String precioProductoSony = driver.findElement(By.cssSelector("css=.col-lg-4:nth-child(1) h5")).getText();
-        //WebElement precioProductoSony = new WebDriverWait(driver, Duration.ofSeconds(10)).until(driver -> driver.findElement(By.cssSelector("body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h5:nth-child(2)")));
-        //System.out.println(precioProductoSony);
-        //assertEquals(precioProductoSony.getText(),"$790");
-
-        //System.out.println(precioProductoSony);
-        //Obtengo el nombre del producto
-        //assertEquals(precioProductoSony.getText(),);
-        //System.out.println(precioProductoSony);
-
-        //div[@id='tbodyid']//div[1]//div[1]//div[1]//h5[1]
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //Doy tiempo a cargar la página
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //Obtengo el precio del primer producto
         String precioVaio = driver.findElement(By.xpath("//div[@id='tbodyid']//div[1]//div[1]//div[1]//h5[1]")).getText();
+        //Muestro en consola para verificar que se carga bien
         System.out.println(precioVaio);
 
-
+        //Doy tiempo a cargar la página
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Una vez que carga el sitio, le doy click al elemento indicado
         primerProductoSonyVaio.click();
-
-        //Guardar precio laptop
-        //WebElement precioPrimerProductoSonyVaio = new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.attributeToBe("css=.price-container"));
-        //WebElement precioVaio = driver.findElement(By.cssSelector("class=\"price-container\""));
-
-
-        //Una vez que carga el sitio, le doy click al elemento indicado
-        //WebElement precioPrimerProductoSonyVaio= new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(""))));
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //String precioVaio = driver.findElement(By.xpath("//div[@id='tbodyid']/h3")).getText();
-        //System.out.println(precioVaio);
 
         //Le agrego un wait para darle tiempo a cargar el sitio
         WebElement primerProductoSonyVaioAddToCart = new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.linkText("Add to cart")));
@@ -96,8 +59,8 @@ public class SeleniumTestLaptop {
         WebElement verCart = new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.id("cartur")));
         //Una vez que carga el sitio, le doy click al elemento indicado
         verCart.click();
-        sleep(2000);
-
+        //sleep(2000);
+        //Cierro el navegador
         driver. quit();
     }
 
